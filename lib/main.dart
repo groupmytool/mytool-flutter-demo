5280529b82edf6e9daea2c36b709be7e3b64b062
HongBo.Zhu <0haizhu0@gmail.com>
2023-08-06 21:46:24 +0800

    认识 StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,24 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(title: const Text("你好 AppBar")),
-      body: const Center(
-        child: Text(
-          "你好 Body",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.red, fontSize: 40),
-        ),
-      ),
+      body: const MyApp(),
     ),
   ));
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "你好 Body",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.red, fontSize: 40),
+      ),
+    );
+  }
+
+}
+
